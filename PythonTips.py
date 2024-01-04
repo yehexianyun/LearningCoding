1d7e8d3d32ca901310dfa610bdc475cdde45c065
xianyunyehe <robortx@gmail.com>
2024-01-04 23:36:05 +0800

    添加Python编程习惯

--- PythonTips.py
+++ PythonTips.py
@@ -1,1 +1,8 @@
-import os
+# 编程建议
+#1. 导入模块中的部分，而不是全部
+from math import sqrt
+#2. 使用PEP 8标准
+#PEP 8是Python官方的编码风格指南，它为Python程序员提供了代码编写的指导方针，主要介绍Python代码的编写规范，是一种编码风格。
+#3. 使用日志输出
+import logging
+logging.debug('debug message')
