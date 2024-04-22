e853f4688a121c595f8c91b02e195741811682a6
xianyunyehe <robortx@gmail.com>
2024-04-22 16:07:20 +0800

    完成代码初步构建

--- 视频剪辑初步.py
+++ 视频剪辑初步.py
@@ -28,7 +28,37 @@ def get_video_duration(video_path):
 
     return float(duration['format']['duration'])
 
-# 示例：获取视频的总时长
-video_path = "G:\\13_Video\\神断狄仁杰[2010][44集].1080P\\神断狄仁杰.第04集.1080P.mp4"
-duration_seconds = get_video_duration(video_path)
-print(f"Video duration: {duration_seconds} seconds")
+
+import os
+filepath = "G:\\等待清单\\神断狄仁杰\\"
+filelist = os.listdir(filepath)
+filelist.pop(0)
+filelist = [filepath + x for x in filelist]
+
+lengthlist = [get_video_duration(x) for x in filelist]
+
+
+def trim_audio(input_file, output_file, start_time, end_time):
+    """
+    使用ffmpeg裁剪音频文件。
+
+    参数:
+    input_file (str): 输入音频文件的路径。
+    output_file (str): 输出音频文件的路径。
+    start_time (int): 裁剪开始的时间，单位为秒。
+    end_time (int): 裁剪结束的时间，单位为秒。
+    """
+    command = [
+        'ffmpeg',
+        '-i', input_file,           # 输入文件
+        '-ss', str(start_time),     # 裁剪开始时间
+        '-to', str(end_time),       # 裁剪结束时间
+        '-c', 'copy',               # 使用相同的编解码器复制音轨
+        output_file                 # 输出文件
+    ]
+    subprocess.run(command, check=True)
+
+
+endlist = [ x-148 for x in lengthlist]
+for i in range(len(filelist)):
+    trim_audio(filelist[i], f"{i}.mp3", 216, endlist[i])
